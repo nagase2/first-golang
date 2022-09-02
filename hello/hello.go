2bc78104d3dcb43b01a7efd43f448db5fc3cc685
Nagase Yoshimasa <yoshimasa_nagase@tsyscom.co.jp>
2022-09-03 01:22:41 +0900

    before Add a test

--- hello/hello.go
+++ hello/hello.go
@@ -3,17 +3,34 @@ package main
 import (
 	"fmt"
 	"log"
+
 	"example.com/greetings"
 )
 
 func main() {
+	// ここで設定することにより先頭のタイムスタンプがなくなり、代わりに指定文字列が入る
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 	// Get a greeting message and print it.
-	message, err := greetings.Hello("sss")
+	//message, err := greetings.Hello("aaa")
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+
+
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	fmt.Println(messages)
+
+	m := make(map[string]int)
+	m["aaa"] = 23
+	m["sss"] = 232
+	m["aaa"] = 231
+	fmt.Println(m["ii"])
 }
